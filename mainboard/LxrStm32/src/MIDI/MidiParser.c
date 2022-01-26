f22f488ef8f05b6ef22834123e9fdc552c778282
Brendan Clarke <bc@10.0.0.14>
2022-01-26 21:35:05 +0100

    fix - midi cc vals are data2

--- mainboard/LxrStm32/src/MIDI/MidiParser.c
+++ mainboard/LxrStm32/src/MIDI/MidiParser.c
@@ -3454,7 +3454,7 @@ void midiParser_MIDIccHandler(MidiMsg msg, uint8_t updateOriginalValue)
             case UNDEF_102: // MOD_WAVE_DRUM* (1-3 only)
 	       if(msg.data1<16)
                {
-                  seq_setNextPattern(msg.data1&0x07,0x7f);
+                  seq_setNextPattern(msg.data2&0x07,0x7f);
                   if(msg.data1>7)
                   {
                      seq_newVoiceAvailable=0x7f;
